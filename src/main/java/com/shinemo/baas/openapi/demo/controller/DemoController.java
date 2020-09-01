d2ad5bc921060dc53e2d4e71fca59326907daf51
鲁超 <luc@shinemo.com>
2020-09-01 15:24:29 +0800

    init

---
+++ src/main/java/com/shinemo/baas/openapi/demo/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.shinemo.baas.openapi.demo.controller;
+
+import com.shinemo.baas.openapi.client.client.ContactClient;
+import com.shinemo.baas.openapi.client.common.ApiResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Create Time:2020/9/1
+ * User: luchao
+ * Email: luc@shinemo.com
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    @Autowired
+    private ContactClient contactClient;
+
+    @GetMapping("/test")
+    public ApiResult test() {
+        return contactClient.pullOfSync(null);
+    }
+}
