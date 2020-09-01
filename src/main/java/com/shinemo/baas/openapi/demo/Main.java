390c970fa63bda158777371b2d39e30829e7b1c2
鲁超 <luc@shinemo.com>
2020-09-01 17:51:01 +0800

    init

--- src/main/java/com/shinemo/baas/openapi/demo/Main.java
+++ src/main/java/com/shinemo/baas/openapi/demo/Main.java
@@ -4,8 +4,6 @@ import com.shinemo.baas.openapi.client.client.ContactClient;
 import com.shinemo.baas.openapi.client.common.ApiResult;
 import com.shinemo.baas.openapi.client.common.BaseConfig;
 
-import javax.swing.*;
-
 /**
  * Create Time:2020/9/1
  * User: luchao
@@ -15,11 +13,11 @@ public class Main {
 
     public static void main(String[] args) {
         // 以下配置需要对接相关开发提供
-        final String domin = "http://baas.uban360.net:21006/";
-        final String id = "1PsfUjyl";
-        final String secret = "Bpf7pQmuuVpxqiO5";
+        final String openApiUri = "http://baas.uban360.net:21006/";
+        final String appId = "1PsfUjyl";
+        final String appSecret = "Bpf7pQmuuVpxqiO5";
 
-        BaseConfig baseConfig = new BaseConfig(domin, id, secret);
+        BaseConfig baseConfig = new BaseConfig(openApiUri, appId, appSecret);
         ContactClient contactClient = new ContactClient(baseConfig);
         ApiResult apiResult = contactClient.pullOfSync(null);
         System.out.println(apiResult.getCode());
