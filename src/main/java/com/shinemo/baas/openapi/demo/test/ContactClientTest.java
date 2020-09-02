6eec9df1e561e086153535e83a3ecdb257357f82
鲁超 <luc@shinemo.com>
2020-09-02 21:54:14 +0800

    init

--- src/main/java/com/shinemo/baas/openapi/demo/test/ContactClientTest.java
+++ src/main/java/com/shinemo/baas/openapi/demo/test/ContactClientTest.java
@@ -3,6 +3,11 @@ package com.shinemo.baas.openapi.demo.test;
 import com.shinemo.baas.openapi.client.client.ContactClient;
 import com.shinemo.baas.openapi.client.common.ApiResult;
 import com.shinemo.baas.openapi.client.common.BaseConfig;
+import com.shinemo.baas.openapi.contact.client.dto.DeptDto;
+import com.shinemo.baas.openapi.contact.client.dto.OrgDto;
+import com.shinemo.baas.openapi.contact.client.dto.UserDto;
+
+import java.util.List;
 
 /**
  * Create Time:2020/9/1
@@ -16,13 +21,39 @@ public class ContactClientTest {
         this.contactClient = new ContactClient(baseConfig);
     }
 
+    public static void main(String[] args) {
+        // 以下配置需要对接相关开发提供
+        final String openApiUri = "http://139.210.243.228:41006/";
+        final String appId = "LadymL6A";
+        final String appSecret = "Z8VqFV7r2RmH4klI";
+
+        BaseConfig baseConfig = new BaseConfig(openApiUri, appId, appSecret);
+        ContactClientTest test = new ContactClientTest(baseConfig);
+        test.pullOfSyncByJson(null);
+    }
+
     /**
      * 拉取通讯录
      *
      * @param orgId 非必传 传了则取莫个单位的通讯录
      */
     public void pullOfSync(Long orgId) {
-        ApiResult apiResult = contactClient.pullOfSync(orgId);
+        ApiResult<List<OrgDto>> apiResult = contactClient.pullOfSync(orgId);
+        if (apiResult.isSuccess()) {
+            List<OrgDto> list = apiResult.getData();
+            for (OrgDto orgDto : list) {
+                for (DeptDto dept : orgDto.getDepts()) {
+                    for (UserDto user : dept.getUsers()) {
+
+                    }
+                }
+            }
+        }
         System.out.println(apiResult.getCode());
+    }
+
+    public void pullOfSyncByJson(Long orgId) {
+        String json = contactClient.pullOfSyncByJson(orgId);
+        System.out.println(json);
     }
 }
