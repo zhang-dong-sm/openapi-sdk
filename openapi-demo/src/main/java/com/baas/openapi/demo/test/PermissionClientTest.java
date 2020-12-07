b3b04a6493793bcf356cd1d211496f4b21fd84f4
dongzhijun <dongzj@shinemo.com>
2020-12-07 13:49:20 +0800

    new code

--- openapi-demo/src/main/java/com/baas/openapi/demo/test/PermissionClientTest.java
+++ openapi-demo/src/main/java/com/baas/openapi/demo/test/PermissionClientTest.java
@@ -1,9 +1,7 @@
 package com.baas.openapi.demo.test;
 
-import com.baas.openapi.client.common.config.ApiResult;
 import com.baas.openapi.client.common.config.BaseConfig;
 import com.baas.openapi.client.permission.PermissionClient;
-import com.baas.openapi.client.permission.dto.DevUserDto;
 
 /**
  * 权限demo
@@ -19,11 +17,11 @@ public class PermissionClientTest {
     private final static BaseConfig baseConfig = new BaseConfig(openApiUri, appId, appSecret);
     private final static PermissionClient permissionClient = new PermissionClient(baseConfig);
 
-    public static void main(String[] args) {
-        DevUserDto devUserDto = new DevUserDto();
-        devUserDto.setDevId(5717888888L);
-        devUserDto.setUid(10104L);
-        ApiResult userPermission = permissionClient.getUserPermission(devUserDto);
-        System.out.println(userPermission);
-    }
+//    public static void main(String[] args) {
+//        DevUserDto devUserDto = new DevUserDto();
+//        devUserDto.setDevId(5717888888L);
+//        devUserDto.setUid(10104L);
+//        ApiResult userPermission = permissionClient.getUserPermission(devUserDto);
+//        System.out.println(userPermission);
+//    }
 }
